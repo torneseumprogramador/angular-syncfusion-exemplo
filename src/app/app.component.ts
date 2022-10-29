1aeb3846416f9e5757abe2388b8d500266732289
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-29 14:02:08 -0300

    sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Diagram, NodeModel } from '@syncfusion/ej2-diagrams';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'syncfusion';
+
+  teste = ()=>{
+    let node: NodeModel = {
+      // Position of the node
+      offsetX: 250,
+      offsetY: 250,
+      // Size of the node
+      width: 100,
+      height: 100,
+      style: {
+        fill: 'red', strokeColor: 'blue', strokeWidth: 5,
+        strokeDashArray: '2 2', opacity: 0.6,
+        textAlign: 'Left'
+      },
+      // Text(label) added to the node
+    };
+
+    // initialize Diagram component
+
+    let diagram: Diagram = new Diagram({
+      width: '100%',
+      height: '600px',
+      // Add node
+      nodes: [node]
+    });
+    // render initialized Diagram
+    diagram.appendTo('#element');
+
+    
+  }
+
 }
